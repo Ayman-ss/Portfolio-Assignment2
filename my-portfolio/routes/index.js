0b0b60ac8c1ffd2ac592ce282706b2169e065c2d
Ayman-ss <ayman.sohail@ontariotechu.net>
2025-11-07 14:32:07 -0500

    Added all working EJS pages and fixed home route

--- my-portfolio/routes/index.js
+++ my-portfolio/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Home
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Home' });
+});
+
+// About
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About Me' });
+});
+
+// Projects
+router.get('/projects', function(req, res) {
+  res.render('projects', { title: 'Projects' });
+});
+
+// Contact
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
